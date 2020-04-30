29480721280dc8c8459806dc4ea2170d2820a88d
Antonio <antonio@WBM-L08.local>
2020-04-30 20:07:42 +0100

    Updates

---
+++ precache-manifest.c02e165cad3d8955a9d5c50cce7cb0b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3d71fb6b4efdae8b1b0336ebfd9be8",
+    "url": "/spain-covid19/index.html"
+  },
+  {
+    "revision": "5539073e9a44abb464e2",
+    "url": "/spain-covid19/static/js/2.00839e30.chunk.js"
+  },
+  {
+    "revision": "d69c2ce72d60c7b8256827601458cc73",
+    "url": "/spain-covid19/static/js/2.00839e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d477d41616b44c740a0",
+    "url": "/spain-covid19/static/js/main.5dc2c4fc.chunk.js"
+  },
+  {
+    "revision": "7a91d9267e4621cd648a",
+    "url": "/spain-covid19/static/js/runtime-main.4040d64d.js"
+  }
+]);
